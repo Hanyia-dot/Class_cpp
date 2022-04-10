365152c14724c42d36ae2048e72803513d26debd
Hanna Dobrowolska <tak.obiektyw.nie@gmail.com>
2022-04-10 17:08:03 +0200

    add lines of code

--- class.cpp
+++ class.cpp
@@ -12,23 +12,33 @@ public:
 int age {};
 int numberOfChildren {};
 
+
+void printMaterialStatus() {
+    cout << "The person is " << (materialStatus?"in a relationship":"single") << endl;
 };
 
+void changematerialStatus(){
+    materialStatus=!materialStatus; // "!" renerses the logical state.
+};
 
-
-
+};
 int main ()
 {
     Person joey;
+    Person hannah;
 
     joey.age = {23}; //after typing "joey." I have only access to public data.
                     // I can't change "materialStatus". It is set by deflaut.
     joey.numberOfChildren = {3};
-    
 
+    hannah.age = {17};
 
-    cout << joey.age << endl;
+    joey.printMaterialStatus();
+    joey.changematerialStatus();
+    joey.printMaterialStatus();
 
+    cout <<  "He in "  << joey.age <<  " years old"  << endl;
+    cout <<  "She is "  << hannah.age <<  " years old"  << endl;
 
     return 0;
 }
